f7e2bc8d4eecffc20d02bb8d74a4a43da5502d59
AmarnathMourougan <dmtoamar@gmail.com>
2024-06-06 09:44:38 +0530

    Variable and Datatypes

---
+++ Day 2/Lab006.py
@@ -0,0 +1,7 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("Your first name is:", firstName)
+print("Your last name is:", lastName)
+
+print("Full Name:", firstName+" "+lastName)
