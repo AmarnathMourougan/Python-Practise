59d71407115e10fefa298442c3b1636ac0b538ec
AmarnathMourougan <dmtoamar@gmail.com>
2024-06-06 09:38:07 +0530

    Delete lab001.py

--- lab001.py
+++
@@ -1,8 +0,0 @@
-class lap01:
-    # Created the method to print the hello statement
-    def helloWorld(self):
-        #print hello world!
-        print ("Hello World!")
-        print (2+2)
-        print(5/2)
-    helloWorld()
